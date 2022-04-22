89ea97d2331149b9671f15314bf79b41912c1350
JZ0ro <103156617+JZ0ro@users.noreply.github.com>
2022-04-22 12:43:55 -0700

    Setting up JUnit commands
    javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java
    java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest

---
+++ MarkdownParseTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class MarkdownParseTest {
+    @Test
+    public void addition() { // Tester method
+        assertEquals(2, 1 + 1); // Determines whether or not our users input equals the expected output of 2
+    }
+}
