9c0a1d6bafda4360d7e2debca7da67f098af44e5
JZ0ro <103156617+JZ0ro@users.noreply.github.com>
2022-04-24 04:33:03 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -7,32 +7,32 @@ import java.util.ArrayList;
 
 public class MarkdownParse {
 
-    public static ArrayList<String> getLinks(String markdown) {
-        ArrayList<String> toReturn = new ArrayList<>();
-        // find the next [, then find the ], then find the (, then read link upto next )
-        int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
-            int openBracket = markdown.indexOf("[", currentIndex);
-            System.out.println(currentIndex);
-            int closeBracket = markdown.indexOf("]", openBracket); 
-            System.out.println(currentIndex);
-            int openParen = markdown.indexOf("(", closeBracket);
-            System.out.println(currentIndex);
-            int closeParen = markdown.indexOf(")", openParen);
-            System.out.println(currentIndex);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+        public static ArrayList<String> getLinks(String markdown) {
+            ArrayList<String> toReturn = new ArrayList<>();
+            // find the next [, then find the ], then find the (, then read link upto next )
+            int currentIndex = 0;
+            while(currentIndex < markdown.length()) {
+                int openBracket = markdown.indexOf("[", currentIndex);
+                System.out.println(currentIndex);
+                int closeBracket = markdown.indexOf("]", openBracket); 
+                System.out.println(currentIndex);
+                int openParen = markdown.indexOf("(", closeBracket);
+                System.out.println(currentIndex);
+                int closeParen = markdown.indexOf(")", openParen);
+                System.out.println(currentIndex);
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
+            }
+
+            return toReturn;
         }
 
-        return toReturn;
-    }
 
-
-    public static void main(String[] args) throws IOException {
-        Path fileName = Path.of(args[0]);
-        String content = Files.readString(fileName);
-        ArrayList<String> links = getLinks(content);
-	    System.out.println(links);
-        System.out.println(links.get(0));
-    }
+        public static void main(String[] args) throws IOException {
+            Path fileName = Path.of(args[0]);
+            String content = Files.readString(fileName);
+            ArrayList<String> links = getLinks(content);
+            System.out.println(links);
+            System.out.println(links.get(0));
+        }
 }
