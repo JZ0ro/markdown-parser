7ad9961f767acf9f4cb75d2854b504c5fe6b0dc9
JZ0ro <103156617+JZ0ro@users.noreply.github.com>
2022-04-24 15:36:30 -0700

    new character sequence o check
    added https sequence to check in link

--- markdownParseReportVersion.java
+++ markdownParseReportVersion.java
@@ -18,10 +18,11 @@ public class markdownParseReportVersion {
             
         }
         CharSequence www = "www";
+        CharSequence https = "https://";
         for (int i = 0; i < toReturn.size(); i++) {
             // loops through links and checks if they have valid URL's
             String link = toReturn.get(i);
-            if (link.contains(www) == true) {
+            if (link.contains(www) == true || link.contains(https) == true) {
                 continue;
             }
             else {
